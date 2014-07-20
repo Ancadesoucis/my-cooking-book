f2fe29c24ca5b0c7fece10bbdde02af835c01894
git <acremyner@gmail.com>
2014-07-20 19:24:51 +0200

    début

---
+++ src/Menu.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class Menu {
+public static void main(String[] args)
+{
+	JFrame fenêtre = new JFrame();
+	fenêtre.setTitle("My Cooking Book");
+	fenêtre.setSize(800, 500);
+	fenêtre.setLocationRelativeTo(null);
+	fenêtre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	fenêtre.setVisible(true);
+}
+
+	
+}
